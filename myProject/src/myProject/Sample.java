6153597376c8a84bfa2610d55a550149bbadc9b3
GreenFrndz <green.frndz@gmail.com>
2017-03-08 23:16:34 +0530

    Sample - play() added

--- myProject/src/myProject/Sample.java
+++ myProject/src/myProject/Sample.java
@@ -3,5 +3,10 @@ package myProject;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Start");
+		Sample s=new Sample();
+		s.play();
+	}
+	public void play(){
+		System.out.println("playing...");
 	}
 }
